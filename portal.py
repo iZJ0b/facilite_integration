9211b5d648e248d5ff406b526501fdd49e2c522e
matheusbarbosa <matheus.barbosa.oliveira00@gmail.com>
2024-10-07 19:56:36 -0300

    update

--- portal.py
+++ portal.py
@@ -10,8 +10,9 @@ def main():
     if 'TOKEN' not in st.session_state:
         st.session_state.TOKEN = autenticacao_facilite()
         st.session_state.lista_empresas = listagem_empresas(st.session_state.TOKEN)
-        st.session_state.export = 0
+        st.session_state.export = True
         st.session_state.valid_export = []
+        st.session_state.layout_texto = ""
     
     st.divider()
     empresas = [f'{empresa["id"]} - {empresa["razaoSocial"]}' for empresa in st.session_state.lista_empresas]
@@ -39,38 +40,37 @@ def main():
             st.checkbox('Notas Recebidas', key='export_nf_recebido_fiscal', disabled=True)
 
         with col_17:
-            if st.button('Exportar', use_container_width=True, icon=':material/exit_to_app:'):
-                st.session_state.export = 1
-
+            if st.button('Gerar Lançamentos', use_container_width=True, icon=":material/sync:"):
                 itens_export = [item for item in st.session_state if 'export_' in item]
                 st.session_state.valid_export = [item for item in itens_export if st.session_state[item]]
                 
+                if st.session_state.valid_export:   # se possui campo selecionado para exportacao
+                    with st.spinner('In progress ...'):
+                        for empresa in options_empresas:    # fazer o loop por empresas que foram selecionados para exportacao
+                            lancamentos_naoClassificados = []
+                            for item_export in st.session_state.valid_export:
+                                
+                                if 'financas_contabil' in item_export:
+                                    texto_layout, naoClassificados = exportar_financas(int(empresa.split(' - ')[0]))
+                                    st.session_state.layout_texto += texto_layout
+                                    lancamentos_naoClassificados.append(naoClassificados)
+                        
+                        st.session_state.export = False
+                else:
+                    st.error('Nenhum item selecionado para exportação')
             
-            if st.button('Voltar', use_container_width=True):
-                pass
-        
-        # mostrar os dados depois de clicar em EXPORTAR
-        if st.session_state.export == 1:
-            if st.session_state.valid_export:   # se possui campo selecionado para exportacao
-                
-                for empresa in options_empresas:    # fazer o loop por empresas que foram selecionados para exportacao
-                    with st.expander(empresa):
-                        for item_export in st.session_state.valid_export:
-                            
-                            if 'financas_contabil' in item_export:
-                                layout_texto, naoClassificados = exportar_financas(int(empresa.split(' - ')[0]))
-
-                                tab1, tab2 = st.tabs(['Exportavel', 'Não Classificados'])
-                                with tab1:
-                                    st.text(layout_texto)
-                                with tab2:
-                                    for iten in naoClassificados:
-                                        st.code(iten)
-                                    # st.write(naoClassificados)
+            if st.download_button('Exportar', 
+                                  data=st.session_state.layout_texto, 
+                                  file_name="export_lancamentos.txt", 
+                                  use_container_width=True, 
+                                  icon=':material/exit_to_app:', 
+                                  disabled=st.session_state.export):
+                st.session_state.layout_texto = ""
+                st.session_state.export = True
+                st.rerun()
 
 
-            else:
-                st.error('Nenhum item selecionado para exportação')
+
 
 
 
