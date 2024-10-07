fdb63ca5c506f9728108d2af5be70d029e9197f0
matheusbarbosa <matheus.barbosa.oliveira00@gmail.com>
2024-10-07 14:24:16 -0300

    update

--- contabil.py
+++ contabil.py
@@ -2,13 +2,13 @@ import requests
 from relacoes_contas.util import plano_contas, bancos
 from layouts import layout_lancContabil
 
-def exportar_financas():
+def exportar_financas(empresa_id):
     contas_contabeis = plano_contas()
     contas_bancos = bancos()
     url = "https://adminbackend.facilite.co/api/financas"
 
     payload = {
-        "empresaId": 8551,
+        "empresaId": empresa_id,
         "dataInicial": "2024-09-01",
         "dataFinal":"2024-09-30",
         "transacoesManuais": False,
@@ -22,8 +22,11 @@ def exportar_financas():
 
     response = requests.request("GET", url, headers=headers, data=payload)
     lancamentos = response.json()
+    
+    # buscar pelo cnpj da empresa
+    cnpj = cnpj_empresa(empresa_id)
+    layout_text = f"|0000|{cnpj}|\n"
 
-    layout_text = f"|0000||\n"
     naoClassificado = []
     for lancamento in lancamentos:
         tipo_movimento = lancamento['tipo']
@@ -41,12 +44,34 @@ def exportar_financas():
             
             # buscar o codigo reduzido da conta bancaria
             id_banco_facilite = lancamento['banco']['id']
-            codReduzido_banco = bancos()[str(id_banco_facilite)]
+            codReduzido_banco = contas_bancos[str(id_banco_facilite)]
 
             text = layout_lancContabil(tipo_movimento, dataPagamento, codReduzido_banco, codReduzido_contrapartida, valor, descricao)
             layout_text += text
-    print()
+    return layout_text
+
+def cnpj_empresa(empresa_id):
+    url = f"https://adminbackend.facilite.co/api/empresas/{empresa_id}"
+    headers = {
+    'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlYXN5am9iIiwiYXV0aCI6IlJPTEVfRU1QUkVTQVMsUk9MRV9FU0NSSVRPUklPX0NPTlRBQklMSURBREUsUk9MRV9GSU5BTkNFSVJPLFJPTEVfRklTQ0FMLFJPTEVfR1JBRklDT1MsUk9MRV9QRVNTT0FMLFJPTEVfUFJPQ0VTU09TLFJPTEVfVVNFUiIsImV4cCI6MTcyOTM2MjA2N30.P_aARH0h07KaJCft68ijSz8qXNTR0yUzHRDpk0ExS_aBvglo248NL-zEdk6sVV5l3-cdUvr0TmBSyuPhEMt1xQ'
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    dados_empresa = response.json()
+    return dados_empresa['cnpj']
+
+def exportar_notas_fiscais():
+    pass
+
+def exportar_imposto():
+    pass
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    exportar_financas()
+    exportar_financas(8551)
+    # cnpj_empresa(8551)
