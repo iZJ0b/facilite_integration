6638bd33a17c4925f5ccebcd05d6f996b32a2d2f
matheusbarbosa <matheus.barbosa.oliveira00@gmail.com>
2025-01-16 16:04:22 -0300

    update

--- authentic_facilite.py
+++ authentic_facilite.py
@@ -18,7 +18,7 @@ def autenticacao_facilite():
     return response.json()['id_token']
 
 def listagem_empresas(token):
-    url = "https://adminbackend.facilite.co/api/empresas/lista-custom/pageable?searchable=&page=0&size=1000&sort=dataCriacao,desc&sort=id&isAtivo=true"
+    url = "https://adminbackend.facilite.co/api/empresas/lista-custom/pageable?searchable=&page=0&size=4000&sort=dataCriacao,desc&sort=id&isAtivo=true"
 
     payload = {}
     headers = {
@@ -34,5 +34,5 @@ def listagem_empresas(token):
 if __name__ == "__main__":
     token = autenticacao_facilite()
     # id_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlYXN5am9iIiwiYXV0aCI6IlJPTEVfRU1QUkVTQVMsUk9MRV9FU0NSSVRPUklPX0NPTlRBQklMSURBREUsUk9MRV9GSU5BTkNFSVJPLFJPTEVfRklTQ0FMLFJPTEVfR1JBRklDT1MsUk9MRV9QRVNTT0FMLFJPTEVfUFJPQ0VTU09TLFJPTEVfVVNFUiIsImV4cCI6MTczMDkxNTI0M30.1g-t7xrwgN6ixRguF4m0eUrsYxXjDh300xtuCARqVOigwv9rP17yRr-uxoTwfqgPQ9JhfLn2Es85_bcc3Rm79Q"
-    # listagem_empresas(token=id_token)
+    listagem_empresas(token=token)
     print()
